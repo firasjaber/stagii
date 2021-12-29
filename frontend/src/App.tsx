cef2fa0bee2cbddae0ddc4e378f701e11ea1146e
Firas Jaber <firasjaberc@gmail.com>
2021-12-29 21:52:58 +0100

    auth context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,20 @@ import MainLayout from './components/MainLayout';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { AuthProvider } from './contexts/useAuth';
 
 function App() {
   return (
     <MantineProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/*' element={<MainLayout />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/*' element={<MainLayout />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </MantineProvider>
   );
 }
---
+++ frontend/src/contexts/useAuth.tsx
@@ -0,0 +1,137 @@
+import axios from 'axios';
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { useLocation } from 'react-router-dom';
+
+const API_URL = 'http://localhost:3000';
+
+interface AuthContextType {
+  // We defined the user type in `index.d.ts`, but it's
+  // a simple object with email, name and password.
+  user?: any;
+  loading: boolean;
+  error?: any;
+  login: (email: string, password: string) => Promise<void>;
+  register: (
+    email: string,
+    firstName: string,
+    lastName: string,
+    type: string,
+    password: string
+  ) => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+
+export function AuthProvider({
+  children,
+}: {
+  children: ReactNode;
+}): JSX.Element {
+  const [token, setToken] = useState<String>();
+  const [user, setUser] = useState<any>();
+  const [error, setError] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (error) setError(null);
+    //eslint-disable-next-line
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const authToken = localStorage.getItem('auth-token');
+    if (authToken) {
+      setToken(authToken);
+      loadUser();
+    }
+    //eslint-disable-next-line
+  }, []);
+
+  const loadUser = async () => {
+    if (!token) return null;
+    const config = {
+      headers: {
+        'content-type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    try {
+      const res = await axios.get(API_URL + '/auth/', config);
+      console.log(res);
+      setUser(res.data.user);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  const login = async (email: string, password: string) => {
+    setLoading(true);
+    const config = {
+      headers: {
+        'content-type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    const body = { email, password };
+    try {
+      const res = await axios.post(API_URL + '/auth/login', body, config);
+      console.log(res);
+      setUser(res.data.user);
+      setToken(res.data.token);
+      localStorage.setItem('auth-token', res.data.token);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  const register = async (
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string,
+    type: string
+  ) => {
+    setLoading(true);
+    const body = { email, firstName, lastName, password, type };
+    const config = {
+      headers: {
+        'content-type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    try {
+      await axios.post(API_URL + '/auth/login', body, config);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  function logout() {
+    setToken('');
+    setUser('');
+    localStorage.removeItem('auth-token');
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ user, error, loading, login, logout, register }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default function useAuth() {
+  return useContext(AuthContext);
+}

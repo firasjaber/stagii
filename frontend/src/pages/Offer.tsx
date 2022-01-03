bf6b3eee48ebfa8ad28da76507f5de76081e1c74
Firas Jaber <firasjaberc@gmail.com>
2022-01-03 22:57:48 +0100

    offers & offer page layout

---
+++ frontend/src/pages/Offer.tsx
@@ -0,0 +1,64 @@
+import { Avatar, Button, Card, Title } from '@mantine/core';
+import React from 'react';
+
+interface Props {}
+
+const Offer = (props: Props) => {
+  const imgUrl =
+    'https://thumbs.dreamstime.com/b/amazon-logo-editorial-vector-illustration-market-136495269.jpg';
+  return (
+    <div>
+      <Title order={2} className='mb-4'>
+        Offer ( id : 0 )
+      </Title>
+      <Card shadow='sm' padding='lg' className='w-full '>
+        <div className='flex flex-row justify-between'>
+          <div className='flex flex-col space-y-3'>
+            <div>
+              <span className='text-gray-400 text-sm'>Company name : </span>{' '}
+              Amazon LLC
+            </div>
+            <div>
+              <span className='text-gray-400 text-sm'>Field : </span> Web
+              Development
+            </div>
+            <div>
+              <span className='text-gray-400 text-sm'>Internship Type : </span>{' '}
+              End of Studies Project
+            </div>
+            <div>
+              <span className='text-gray-400 text-sm'>Posted at : </span> 12 Jun
+              2001
+            </div>
+
+            <div className='text-green-500'>
+              <span className='text-gray-400 text-sm'>Still Open : </span> Yes
+            </div>
+          </div>
+          <div>
+            <Avatar size={150} className='rounded-full m-4' src={imgUrl} />
+          </div>
+        </div>
+        <div className='mt-3'>
+          <div>
+            <span className='text-gray-400 text-sm'>Description : </span>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam odio
+            quasi omnis odit qui beatae corporis quod explicabo voluptate.
+          </p>
+          <div className='flex items-center space-x-2 mt-4'>
+            <Button variant='outline' color='green'>
+              Submit
+            </Button>
+            <Button variant='outline' color='red'>
+              Delete
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Offer;
--- frontend/src/pages/Offers.tsx
+++ frontend/src/pages/Offers.tsx
@@ -1,9 +1,95 @@
+import { Avatar, Button, SimpleGrid, Title } from '@mantine/core';
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 interface Props {}
 
+const data = [
+  {
+    id: 0,
+    companyName: 'Incedo GmbH',
+    internshipField: 'Web Development',
+    internshipType: 'PFE',
+  },
+  {
+    id: 0,
+    companyName: 'Incedo GmbH',
+    internshipField: 'Web Development',
+    internshipType: 'Perfection',
+  },
+  {
+    id: 0,
+    companyName: 'Amazon LLC',
+    internshipField: 'Digital Markenting',
+    internshipType: 'PFE',
+  },
+  {
+    id: 0,
+    companyName: 'Netflix CO',
+    internshipField: 'Web Development',
+    internshipType: 'PFE',
+  },
+  {
+    id: 0,
+    companyName: 'Meta Corp',
+    internshipField: 'Devops',
+    internshipType: 'PFE',
+  },
+];
+
 const Offers = (props: Props) => {
-  return <div>offers page</div>;
+  return (
+    <div>
+      <Title order={2} className='mb-4'>
+        Offers
+      </Title>
+      <SimpleGrid
+        cols={4}
+        breakpoints={[
+          { maxWidth: 'xl', cols: 4, spacing: 'md' },
+          { maxWidth: 'lg', cols: 3, spacing: 'md' },
+          { maxWidth: 'md', cols: 2, spacing: 'md' },
+          { maxWidth: 'sm', cols: 1, spacing: 'sm' },
+          { maxWidth: 'xs', cols: 1, spacing: 'sm' },
+        ]}
+      >
+        {data.map((student) => (
+          <OfferCard
+            id={student.id}
+            companyName={student.companyName}
+            internshipField={student.internshipField}
+            internshipType={student.internshipType}
+          />
+        ))}
+      </SimpleGrid>
+    </div>
+  );
 };
 
+interface OfferCardProps {
+  id: Number;
+  companyName: string;
+  internshipField: string;
+  internshipType: string;
+}
+
+const OfferCard = (props: OfferCardProps) => {
+  const navigate = useNavigate();
+  return (
+    <div className='bg-white shadow p-6 flex flex-col items-center'>
+      <Avatar size={120} className='rounded-full' />
+      <div className='mt-2 mb-1 font-semibold text-2xl'>
+        {props.companyName}
+      </div>
+      <div className='my-2 text-lg'>{props.internshipField}</div>
+      <div className='mb-3 text-gray-500'>{props.internshipType}</div>
+      <Button
+        variant={'outline'}
+        onClick={() => navigate('/offers/' + props.id)}
+      >
+        View details
+      </Button>
+    </div>
+  );
+};
 export default Offers;

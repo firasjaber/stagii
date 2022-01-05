4d5f83b0adda017109ba5bb9b0c83e7a049b14e4
Firas Jaber <firasjaberc@gmail.com>
2022-01-05 23:40:13 +0100

    refactoring

--- frontend/src/components/NavbarList.tsx
+++ frontend/src/components/NavbarList.tsx
@@ -10,8 +10,9 @@ import {
 } from '@heroicons/react/solid';
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useAuth from '../contexts/useAuth';
 
-const pages = [
+const superNavlist = [
   {
     href: '/',
     title: 'Home',
@@ -70,12 +71,119 @@ const pages = [
   },
 ];
 
+const etudNavList = [
+  {
+    href: '/',
+    title: 'Home',
+    Icon: (
+      <OfficeBuildingIcon className='h-6 w-6 bg-yellow-200 text-yellow-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/students',
+    title: 'Students',
+    Icon: (
+      <UsersIcon className='h-6 w-6 bg-blue-200 text-blue-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/companies',
+    title: 'Companies',
+    Icon: (
+      <LibraryIcon className='h-6 w-6 bg-teal-200 text-teal-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/offers',
+    title: 'Offers',
+    Icon: (
+      <FolderOpenIcon className='h-6 w-6 bg-green-200 text-green-400 p-[3px] rounded-sm' />
+    ),
+  },
+
+  {
+    href: '/tickets',
+    title: 'Tickets',
+    Icon: (
+      <DocumentIcon className='h-6 w-6 bg-sky-200 text-sky-700 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/notifications',
+    title: 'Notifications',
+    Icon: (
+      <AnnotationIcon className='h-6 w-6 bg-violet-200 text-violet-400 p-[3px] rounded-sm' />
+    ),
+  },
+];
+
+const administrativeNavlist = [
+  {
+    href: '/',
+    title: 'Home',
+    Icon: (
+      <OfficeBuildingIcon className='h-6 w-6 bg-yellow-200 text-yellow-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/students',
+    title: 'Students',
+    Icon: (
+      <UsersIcon className='h-6 w-6 bg-blue-200 text-blue-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/companies',
+    title: 'Companies',
+    Icon: (
+      <LibraryIcon className='h-6 w-6 bg-teal-200 text-teal-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/offers',
+    title: 'Offers',
+    Icon: (
+      <FolderOpenIcon className='h-6 w-6 bg-green-200 text-green-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/submits',
+    title: 'Submits',
+    Icon: (
+      <FolderAddIcon className='h-6 w-6 bg-green-200 text-green-400 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/tickets',
+    title: 'Tickets',
+    Icon: (
+      <DocumentIcon className='h-6 w-6 bg-sky-200 text-sky-700 p-[3px] rounded-sm' />
+    ),
+  },
+  {
+    href: '/notifications',
+    title: 'Notifications',
+    Icon: (
+      <AnnotationIcon className='h-6 w-6 bg-violet-200 text-violet-400 p-[3px] rounded-sm' />
+    ),
+  },
+];
+
 interface Props {}
 
 const NavbarList = (props: Props) => {
+  const { user } = useAuth();
+  let pages: any = [];
+  if (user.type === 'STUDENT') {
+    pages = etudNavList;
+  } else if (user.type === 'EMPLOYER') {
+    pages = administrativeNavlist;
+  } else {
+    pages = superNavlist;
+  }
   return (
     <div className='font-sans'>
-      {pages.map(({ href, title, Icon }, idx) => (
+      {pages.map(({ href, title, Icon }: any, idx: any) => (
         <ListItem href={href} title={title} Icon={Icon} key={idx} />
       ))}
     </div>
